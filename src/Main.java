9e98c29df24146e50bd3b360e510306953162cd3
sizlo <t.brier93@gmail.com>
2017-06-14 20:37:12 +0100

    Play the game

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@
  */
 public class Main {
     public static void main(String[] args) {
-        // Colour printing
-        // https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
-        System.out.println("\u001B[31mHello \u001B[32mWorld\u001B[0m!");
+        IUserInterface ui = new ConsoleUI();
+        Game game = new Game(ui);
+        game.Play();
     }
 }
