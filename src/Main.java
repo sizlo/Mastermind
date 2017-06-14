b6d594a2e8b6cf4515639275585ad646c6e66931
sizlo <t.brier93@gmail.com>
2017-06-14 18:59:29 +0100

    Add reference for colour printing

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Colour printing
+        // https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
+        System.out.println("\u001B[31mHello \u001B[32mWorld\u001B[0m!");
     }
 }
